c90b4b5fed0fe8a88cddbc7b0df1a2035cd6c606
fereshtehbaradaran <bara.frsht@gmail.com>
2023-01-01 15:22:33 +0330

    master changed

--- HW4/Master/src/main.cpp
+++ HW4/Master/src/main.cpp
@@ -2,17 +2,14 @@
 #include <Wire.h>
 
 void setup(){
-    Wire.begin();
-    Serial.begin(9600);
+    Wire.begin(8);
+    Wire.onRequest(requestEvent);
 }
 
 void loop(){
-    Wire.requestFrom(8, 6);
-
-    while (Wire.available()){
-        char c = Wire.read();
-        Serial.print(c);
-    }
-
     delay(500);
+}
+
+void requestEvent(){
+    Wire.write("hello ");
 }
