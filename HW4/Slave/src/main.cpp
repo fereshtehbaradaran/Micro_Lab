0ab3e5a050b88ba1f468b8c601702fee57361492
fereshtehbaradaran <bara.frsht@gmail.com>
2023-01-01 15:49:05 +0330

    slave code added

--- HW4/Slave/src/main.cpp
+++ HW4/Slave/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Wire.begin(6);                
+  Wire.onReceive(receiveEvent); 
+  Serial.begin(9600);           
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(100);
+}
+
+
+void receiveEvent(int howMany) {
+
+  while(1 < Wire.available()) {
+    char c = Wire.read();
+    Serial.print(c); 
+  }
+
 }
